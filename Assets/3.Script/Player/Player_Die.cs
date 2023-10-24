ee84a2f4216e7cea2b6c824e5580554fba9bac3c
0Baek <wlaos45@naver.com>
2023-10-24 17:23:06 +0900

    [기능추가] R키 재시작

--- Assets/3.Script/Player/Player_Die.cs
+++ Assets/3.Script/Player/Player_Die.cs
@@ -12,4 +12,13 @@ public class Player_Die : MonoBehaviour
             gameObject.SetActive(false);//Player 비활성화
         }
     }
+    public void Die()
+    {
+        if (GameManager.FindObjectOfType<GameManager>().TryGetComponent(out GameManager gm))//죽었을 때 R키를 누르시오 
+        {
+            gm.EndGame(); //게임 매니저로 이동 
+        }
+    }
+    //2023/10/24 박준영 
+
 }
