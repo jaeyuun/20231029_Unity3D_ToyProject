b8f4078b7203b0882667472f5204b86dc0f12c94
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-10-24 19:52:09 +0900

    [리팩토링] Map_Move 11줄 변경

--- Assets/3.Script/ECT/Map_Move.cs
+++ Assets/3.Script/ECT/Map_Move.cs
@@ -8,10 +8,10 @@ using UnityEngine;
 public class Map_Move : MonoBehaviour
 {
     private float position_X;//포지션설정
-    private float Speed = 5f; //움직이는 스피드
+    private float Speed = -5f; // 오른쪽으로 이동하려면 이 값을 양수로 변경하세요
     private void FixedUpdate()
     {
-        position_X = transform.position.x + (Speed * Time.fixedDeltaTime)*-1;
+        position_X = transform.position.x + (Speed * Time.fixedDeltaTime);
         transform.position = new Vector3(position_X, transform.position.y, 0f);
     }
 }
