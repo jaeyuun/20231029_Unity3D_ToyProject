113e22b26fc196fc9fb83b46c8582bd266a61c7d
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-10-24 16:04:48 +0900

    [기능추가] 파이프스포너추가
    사용방법
    Map에 추가하여 파이프 프리팹을 넣어준다.
    Min과 Max값을 지정하여 파이프의 위치를 랜덤생성한다.

--- Assets/3.Script/ECT/Map_Move.cs
+++ Assets/3.Script/ECT/Map_Move.cs
@@ -8,6 +8,6 @@ public class Map_Move : MonoBehaviour
     private void FixedUpdate()
     {
         position_X -= 0.1f;
-        transform.position = new Vector3(position_X, 0f, 0f);
+        transform.position = new Vector3(position_X, transform.position.y, 0f);
     }
 }
