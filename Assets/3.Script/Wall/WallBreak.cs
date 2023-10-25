0ddc9c4076d650ee21ce39f9db726916cd1c32d7
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-10-25 10:21:23 +0900

    [Fix] 버그 수정 내용참조
    메인에 문제가 생겨서 리엑트 진행
    파이프
    - isisKinematic 적용하여 플레이어에 닿으면 중력 적용
    - 스폰후 5초 이후 삭제
    - 속도 2배증가
    - Wall tag 적용하여 플레이어 게임오버 적용

--- Assets/3.Script/Wall/WallBreak.cs
+++ Assets/3.Script/Wall/WallBreak.cs
@@ -7,9 +7,9 @@ public class WallBreak : MonoBehaviour
     // [JaeYun] WallBreak: 벽이 무적상태의 플레이어와 닿았을 때 사용
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))//추가 해주세요 조건 플레이어가 자이언트 아이템을 먹었을 때 is키네틱을 false로 바꿔주세요
         {
-            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 }
