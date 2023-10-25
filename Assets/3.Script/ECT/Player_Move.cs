1cdf6524038526c5dfcd568854d50f958865fe5a
0Baek <148294815+0Baek@users.noreply.github.com>
2023-10-25 19:07:36 +0900

    [Feat] isGodmode 실행시 플레이어 속도 추가+wall 튕겨나가게 박준영

--- Assets/3.Script/ECT/Player_Move.cs
+++ Assets/3.Script/ECT/Player_Move.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 
 //생성된 파이프가 움직이는 코드
-public class Map_Move : MonoBehaviour
+public class Player_Move : MonoBehaviour
 {
     private float position_X;//포지션설정
-    private float Speed = 7f; // 오른쪽으로 이동하려면 이 값을 양수로 변경하세요
+    public  float Speed = 7f; // 오른쪽으로 이동하려면 이 값을 양수로 변경하세요
     private void FixedUpdate()
     {
         position_X = transform.position.x + (Speed * Time.fixedDeltaTime);
